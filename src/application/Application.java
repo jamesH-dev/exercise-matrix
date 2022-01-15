5f0e10688128cb0af1e62314b27269633a261492
jamesH-dev <jamesrpm@hotmail.com>
2022-01-15 01:11:56 -0300

    Finalização Exercício 01

---
+++ src/application/Application.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Fazer um programa para ler um número inteiro N e uma matriz de
+ * ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+ * principal e a quantidade de valores negativos da matriz.
+ */
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int negativeNumbers = 0;
+        List<Integer> mainDiagonal = new ArrayList<>();
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                mat[i][j] = sc.nextInt();
+                if (mat[i][j] < 0){
+                    negativeNumbers++;
+                }
+                if (i == j){
+                    mainDiagonal.add(mat[i][j]);
+                }
+            }
+        }
+
+        System.out.println("Main Diagonal: ");
+        for (int mainD : mainDiagonal)
+            System.out.print(mainD + " ");
+
+        System.out.println("\nNegative Numbers: " + negativeNumbers);
+
+        sc.close();
+
+
+
+    }
+}
